0f128c9ad17c01b9d9e33edf40f3f70971d2b002
Ravikumar <ravikumarjupudi143@gmail.com>
2026-02-05 11:58:58 +0530

    commit3

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=jhon
+person2=Raheem
+
+echo "$person1:: Hi suresh how are you"
+echo "$person2:: Hi Hello ramesh im doing good how are you"
+echo "$person1:: whatb are you learning"
+echo "$person2:: learning shell practice"
