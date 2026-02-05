8a8381c88ea440ebf65b75fab2717c4270046bb1
Ravikumar <ravikumarjupudi143@gmail.com>
2026-02-05 11:28:14 +0530

    second commit2

---
+++ 02-converstion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Ramesh:: Hi suresh how are you"
+echo "Suresh:: Hi Hello ramesh im doing good how are you"
+echo " Ramesh whatb are you learning"
+echo " Suresh learning shell practice"
+``
