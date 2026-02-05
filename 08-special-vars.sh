c57af2b24a0def7bebc88b1cf5427221f78026e1
Ravikumar <ravikumarjupudi143@gmail.com>
2026-02-05 17:26:28 +0530

    varables5

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,6 @@
 echo "all variables pass to the script: $@"
 echo "all variables pass to the script: $*"
 echo "script name: $0"
+echo " current working directary: $PWD"
+echo "who is running this: $USER"
+echo "home directary of user: $HOME"
