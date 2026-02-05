f7cce9a6c2c93d41332271662a47d0533cc20e33
Ravikumar <ravikumarjupudi143@gmail.com>
2026-02-05 14:07:04 +0530

    commit3

---
+++ 04-varables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1:: welcome to alaska $person2"
+echo "putin:: thank you $person1 finally no an zoom webinar"
+echo "trump:: so do we have a deal"
+echo "$person2:: yes if we make ukrain join russia"
